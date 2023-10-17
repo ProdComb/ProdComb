357044160163f1ab857ca4ad95e57a47f3953ab7
Mazhar Hussain <realmazharhussain@gmail.com>
2023-10-17 13:54:43 +0500

    Configure django project to use MySQL

--- ProdComb/settings.py
+++ ProdComb/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'ProdComb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ProdComb',
+        'USER': 'django',
+        'PASSWORD': (BASE_DIR / 'password').read_text().strip(),
     }
 }
 
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Karachi'
 
 USE_I18N = True
 
