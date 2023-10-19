4ece92ab154e336140c09dd1056ae0601010a308
Mazhar Hussain <realmazharhussain@gmail.com>
2023-10-19 09:56:00 +0500

    Add Category model

--- main/models.py
+++ main/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     class Type(models.TextChoices):
         SOFTWARE = "sw"
