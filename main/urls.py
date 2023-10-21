5416c4ecb6571800a135e434c693cc97ae7a4a65
Mazhar Hussain <realmazharhussain@gmail.com>
2023-10-21 11:56:17 +0500

    Add basic views for models

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('combinations/', views.Combinations.as_view(), name="combinations"),
+    path('combinations/<int:pk>/', views.CombinationDetail.as_view(), name="combination_detail"),
+    path('categories/', views.Categories.as_view(), name="categories"),
+    path('categories/<int:pk>/', views.CategoryDetail.as_view(), name="category_detail"),
+    path('products/', views.Products.as_view(), name="products"),
+    path('products/<int:pk>/', views.ProductDetail.as_view(), name="product_detail"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,37 @@
 from django.shortcuts import render
+from django.views import generic
+
+from . import models
 
 def index(request):
     return render(request, "index.html")
+
+class Combinations(generic.ListView):
+    template_name = "combinations.html"
+    context_object_name = "combinations"
+    model = models.Combination
+
+class CombinationDetail(generic.DetailView):
+    template_name = "combination-detail.html"
+    context_object_name = "combination"
+    model = models.Combination
+
+class Categories(generic.ListView):
+    template_name = "categories.html"
+    context_object_name = "categories"
+    model = models.Category
+
+class CategoryDetail(generic.DetailView):
+    template_name = "category-detail.html"
+    context_object_name = "category"
+    model = models.Category
+
+class Products(generic.ListView):
+    template_name = "products.html"
+    context_object_name = "products"
+    model = models.Product
+
+class ProductDetail(generic.DetailView):
+    template_name = "product-detail.html"
+    context_object_name = "product"
+    model = models.Product
