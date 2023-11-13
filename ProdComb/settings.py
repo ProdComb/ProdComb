05b8ec85a2d0b14500ecaddaad8d217b669018e1
Mazhar Hussain <realmazharhussain@gmail.com>
2023-11-13 18:01:10 +0500

    Add 192.168.42.5 to ALLOWED_HOSTS list

--- ProdComb/settings.py
+++ ProdComb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--b8h5pwe8i2q@$y!4x24@vb%uh1mkdiwz%lbc25=pe5@wy$3-2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "192.168.43.167"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "192.168.43.167", "192.168.42.5"]
 
 
 # Application definition
